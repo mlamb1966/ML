c689c1095bec6ff1a3af95b11199e0d1d1e99106
Martin Lamb <martin.lamb@coreazure.com>
2020-05-07 12:00:41 +0100

    removed most customisations except temp drive

--- AIB/customiseimage.ps1
+++ AIB/customiseimage.ps1
@@ -6,21 +6,21 @@ New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 
 
 #Install VSCode
-Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
-Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
+#Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
+#Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
 
 #Start sleep
-Start-Sleep -Seconds 10
+#Start-Sleep -Seconds 10
 
 #InstallNotepadplusplus
-Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
-Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
+#Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
+#Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
 
 #Start sleep
-Start-Sleep -Seconds 10
+#Start-Sleep -Seconds 10
 
 #InstallFSLogix
-Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
-Start-Sleep -Seconds 10
-Expand-Archive -Path 'C:\temp\fslogix.zip' -DestinationPath 'C:\temp\fslogix\'  -Force
-Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
+#Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
+#Start-Sleep -Seconds 10
+#Expand-Archive -Path 'C:\temp\fslogix.zip' -DestinationPath 'C:\temp\fslogix\'  -Force
+#Invoke-Expression -Command 'C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart'
